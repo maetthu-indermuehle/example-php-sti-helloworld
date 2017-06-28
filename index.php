0de021fd8fb6528ae8246a04255762acb838e4f5
Matthias Indermuehle <matthias.indermuehle@vshn.ch>
2017-06-28 17:26:18 +0200

    Small change

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
     <div class="container">
 
       <div class="starter-template">
-        <h1>Hallo <?php echo 'APPUiO'?> by VSHN</h1>
+        <h1>Hallo <?php echo 'APPUiO'?></h1>
         <p class="lead">APPUiO Example Source To Image PHP</p>
         <p>The name of this pod is <?php echo gethostname(); ?> </p>
       </div>
